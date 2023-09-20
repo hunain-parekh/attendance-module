e23c833c6148869c40d1ef0c47c90a8c261c606a
Hunain Parekh <hunain.parekh@xloopdigital.com>
2023-09-20 12:05:48 +0500

    3 tables seeds done

---
+++ database/factories/ShiftFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Shift>
+ */
+class ShiftFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'start_time' => fake()->time(),
+            'end_time' => fake()->time(),
+        ];
+    }
+}
